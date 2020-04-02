35134723ee320f910a29715cfb3105b27e80db16
Syed Qadri <rqadri133@gmail.com>
2020-04-02 18:45:21 -0400

    Connection String Azure Cloud sql Set up

--- BlueKangrooCoreOnlyAPI/Startup.cs
+++ BlueKangrooCoreOnlyAPI/Startup.cs
@@ -121,6 +121,12 @@ namespace BlueKangrooCoreOnlyAPI
 
 
           });
+
+
+            services.AddControllers()
+              .AddNewtonsoftJson(options =>
+              options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+               );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
